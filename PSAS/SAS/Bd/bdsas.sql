56fe5722512a4b05b1bf325cbab6fe80fa18c253
neimar <neimarmg@hotmail.com>
2014-12-16 12:33:51 -0200

    2014 12 16 12:33 Edição na base de dados

--- PSAS/SAS/Bd/bdsas.sql
+++ PSAS/SAS/Bd/bdsas.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2014 at 01:02 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: 16-Dez-2014 às 15:31
+-- Versão do servidor: 5.5.39
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,58 +23,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cidades`
+-- Estrutura da tabela `cidades`
 --
 
 CREATE TABLE IF NOT EXISTS `cidades` (
-	`codCidade` int(11) NOT NULL,
-	`DATACRIACAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USERCRICACAO` varchar(50) NOT NULL,
-	`DATAEDICAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USEREDICAO` varchar(50) NOT NULL
+`CODCIDADE` int(11) NOT NULL,
+  `NOME` varchar(100) NOT NULL,
+  `DATACRIACAO` datetime NOT NULL,
+  `USERCRICACAO` varchar(50) NOT NULL,
+  `DATAEDICAO` datetime DEFAULT NULL,
+  `USEREDICAO` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `niveisusuarios`
+-- Estrutura da tabela `niveisuser`
 --
 
-CREATE TABLE IF NOT EXISTS `niveisusuarios` (
-	`codNivelUsuario` int(11) NOT NULL,
-	`DATACRIACAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USERCRICACAO` varchar(50) NOT NULL,
-	`DATAEDICAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USEREDICAO` varchar(50) NOT NULL
+CREATE TABLE IF NOT EXISTS `niveisuser` (
+`CODNIVELUSER` int(11) NOT NULL,
+  `NOME` varchar(100) NOT NULL,
+  `DATACRIACAO` datetime NOT NULL,
+  `USERCRICACAO` varchar(50) NOT NULL,
+  `DATAEDICAO` datetime NOT NULL,
+  `USEREDICAO` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipoutilitario`
+-- Estrutura da tabela `tipoutilitarios`
 --
 
-CREATE TABLE IF NOT EXISTS `tipoutilitario` (
-	`codTipoUtilitario` int(11) NOT NULL,
-	`DESC` varchar(100) NOT NULL,
-	`DATACRIACAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USERCRICACAO` varchar(50) NOT NULL,
-	`DATAEDICAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USEREDICAO` varchar(50) NOT NULL
+CREATE TABLE IF NOT EXISTS `tipoutilitarios` (
+`CODTIPOUTILITARIO` int(11) NOT NULL,
+  `NOME` varchar(100) NOT NULL,
+  `DATACRIACAO` datetime NOT NULL,
+  `USERCRICACAO` varchar(50) NOT NULL,
+  `DATAEDICAO` datetime NOT NULL,
+  `USEREDICAO` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-	`codUsuario` int(11) NOT NULL,
-	`DATACRIACAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USERCRICACAO` varchar(50) NOT NULL,
-	`DATAEDICAO` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`USEREDICAO` varchar(50) NOT NULL
+`CODUSER` int(11) NOT NULL,
+  `NOME` varchar(100) NOT NULL,
+  `DATACRIACAO` datetime NOT NULL,
+  `USERCRICACAO` varchar(50) NOT NULL,
+  `DATAEDICAO` datetime NOT NULL,
+  `USEREDICAO` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -85,25 +88,25 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Indexes for table `cidades`
 --
 ALTER TABLE `cidades`
- ADD PRIMARY KEY (`codCidade`);
+ ADD PRIMARY KEY (`CODCIDADE`);
 
 --
--- Indexes for table `niveisusuarios`
+-- Indexes for table `niveisuser`
 --
-ALTER TABLE `niveisusuarios`
- ADD PRIMARY KEY (`codNivelUsuario`);
+ALTER TABLE `niveisuser`
+ ADD PRIMARY KEY (`CODNIVELUSER`);
 
 --
--- Indexes for table `tipoutilitario`
+-- Indexes for table `tipoutilitarios`
 --
-ALTER TABLE `tipoutilitario`
- ADD PRIMARY KEY (`codTipoUtilitario`);
+ALTER TABLE `tipoutilitarios`
+ ADD PRIMARY KEY (`CODTIPOUTILITARIO`);
 
 --
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
- ADD PRIMARY KEY (`codUsuario`);
+ ADD PRIMARY KEY (`CODUSER`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -113,22 +116,22 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `cidades`
 --
 ALTER TABLE `cidades`
-MODIFY `codCidade` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `CODCIDADE` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `niveisusuarios`
+-- AUTO_INCREMENT for table `niveisuser`
 --
-ALTER TABLE `niveisusuarios`
-MODIFY `codNivelUsuario` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `niveisuser`
+MODIFY `CODNIVELUSER` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tipoutilitario`
+-- AUTO_INCREMENT for table `tipoutilitarios`
 --
-ALTER TABLE `tipoutilitario`
-MODIFY `codTipoUtilitario` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tipoutilitarios`
+MODIFY `CODTIPOUTILITARIO` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-MODIFY `codUsuario` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `CODUSER` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
