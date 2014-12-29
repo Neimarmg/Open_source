60f7e92913d187abfad58992e05047445fe5c21d
neimar <neimarmg@hotmail.com>
2014-12-29 20:52:40 -0200

    2014 12 29 20:40 Edição de css e da

--- PSAS/SAS/app/main.php
+++ PSAS/SAS/app/main.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <?php
-	//require_once 'class/conexao.php';
+	require_once '../class/conexao.php';
 ?>
 
 <html>
@@ -8,7 +8,9 @@
 	<title>Tela de loguin de usuário</title>
 	<meta charset="ISO-8859-1">
 	<style type="text/css"> @import "../css/global.css" </style>
+	<style type="text/css"> @import "../css/fontes.css" </style>
 	<style type="text/css"> @import "../css/login.css" </style>
+
 	
 </head>
 
