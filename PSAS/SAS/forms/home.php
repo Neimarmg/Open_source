5d4c2da6ad604e62b2a1b820eb7160849d50bf5f
Neimar <neimarmg@hotmail.com>
2015-07-30 06:57:33 -0300

    2015 07 30

--- PSAS/SAS/forms/home.php
+++ PSAS/SAS/forms/home.php
@@ -5,28 +5,78 @@
 
 <html>
 <head>
-	<title>Tela de loguin de usuário</title>
-	<meta charset="ISO-8859-1">
-	<style type="text/css"> @import "../css/global.css" </style>
-	<style type="text/css"> @import "../css/fontes.css" </style>
-	<style type="text/css"> @import "../css/home.css" </style>
+	<title>Migração de database</title>
 </head>
-
-<body>
+<body>	
 	<main>
-		<section>
-			
+		<section id='cCidades'>
+			<fieldset>
+	
+				<h3>MIgração de dados</h3>
+				
+				<form method="post" 
+					action="?go=migrar"
 					
-			
-			</form>
-		
-		</section>
-		
-
+				>
+					
+					<br/> <br/> 
+										
+					<label for="Nome">Nome da cidade:</label> 
+					<input  name="nome" id="nome" type="text" required/> 				
+					
+					<br/> <br/> <br/> 
+					
+					<label for="">Unidade federativa:</label> 
+					<select name="codUf" id="codUf" type="checkbox" required>
+						<option value="0">...</option>
+						<?php 						
+							$sql = "select codUf, uf, estado from uf order by uf"; 
+							$result = mysql_query($sql);
+							while ($fila = mysql_fetch_row($result)) {
+								echo "<option value=".$fila['0'].">".$fila['1']." - ".$fila['2']."</option>";
+							}
+						?>
+					</select>	
+							
+					<br/> 
+					<button  name="Migrar" id="botton" type="text"/>	
+				
+				</form>
+			</fieldset>			
+		</section>	
 	</main>
-	<footer>
-		<p>Todas os registro aqui são propriedade de seus respectivos donos. <br> ©2014 APL soluções. Todos os direitos reservados. </p>
-	</footer>
 </body>
 </html>
 
+<?php	
+		
+	if (@$_GET['go'] == 'migrar'){
+		
+		/*Variavel contatenação de string para parametro de sql*/
+		$filtro = utilitario::contatena($nome.$codUf);
+		
+		/*Validação de campos obrigatórios não prenchidos*/
+		if (validadaDados::$Contador > 0) {
+			validadaDados::aletaNaoPrechimento(true);
+		
+		}else{			
+
+			/*Controle de duplicação de registros*/
+			if (validadaDados::resultQuery(mysql_num_rows(mysql_query("SELECT *FROM `cidades` LEFT JOIN UF ON cidades.`codUf`= uf.codUf WHERE CONCAT(cidades.nome,cidades.`codUf`) = '$filtro'"))) == 0) {
+
+				@mysql_query("INSERT INTO `cidades`(`codCidade`, `codUf`, `nome`) VALUES (NULL,'$codUf','$nome')");
+			
+				/*Confirmação de inserção de registro*/
+				if (validadaDados::resultQuery(mysql_num_rows(mysql_query("SELECT *FROM `cidades` LEFT JOIN UF ON cidades.`codUf`= uf.codUf WHERE CONCAT(cidades.nome,cidades.`codUf`) = '$filtro'"))) == 1) {
+					validadaDados::registradoComSucesso(true);
+				
+				}else{
+					validadaDados::registroNaoInserido(true);
+				}
+			}else{
+				validadaDados::registroExitente(true);
+			}
+		}
+		
+	}	
+?>
